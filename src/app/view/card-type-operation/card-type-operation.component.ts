b48edb0ceb5495921b748ca0b22e0b8e2e3ec212
NOSTRA <akaabouroussama@gmail.com>
2021-05-19 16:56:06 +0100

    lhafid llah v4

---
+++ src/app/view/card-type-operation/card-type-operation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-type-operation',
+  templateUrl: './card-type-operation.component.html',
+  styleUrls: ['./card-type-operation.component.css']
+})
+export class CardTypeOperationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
