01f75bf93b260ddebbb3e68e16c2e2399b4a7cb1
NOSTRA <akaabouroussama@gmail.com>
2021-05-19 16:55:30 +0100

    lhafid llah v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
+
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {FactureCreateComponent} from './view/facture-create/facture-create.component';
@@ -8,17 +9,16 @@ import {HttpClientModule} from '@angular/common/http';
 import {FactureCriteriaComponent} from './view/facture-criteria/facture-criteria.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FactureListComponent} from './view/facture-list/facture-list.component';
-import { ViewComponent } from './view/view.component';
+import {ViewComponent} from './view/view.component';
 import {FactureJournalComponent} from './view/facture-journal/facture-journal.component';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
-import {DeclarationTvasaveComponent} from "./view/declaration-tvasave/declaration-tvasave.component";
-import {DecltvacriteriaComponent} from "./view/decltvacriteria/decltvacriteria.component";
-import {PopupFactureComponent} from "./view/popup-facture/popup-facture.component";
-import { OperationSocieteSaveComponent } from './view/operation-societe-save/operation-societe-save.component';
-import { TypeOperationSaveComponent } from './view/type-operation-save/type-operation-save.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {DeclarationTvasaveComponent} from './view/declaration-tvasave/declaration-tvasave.component';
+import {DecltvacriteriaComponent} from './view/decltvacriteria/decltvacriteria.component';
+import {PopupFactureComponent} from './view/popup-facture/popup-facture.component';
+import {TypeOperationSaveComponent} from './view/type-operation-save/type-operation-save.component';
+import { CardTypeOperationComponent } from './view/card-type-operation/card-type-operation.component';
 
 
 @NgModule({
@@ -32,8 +32,10 @@ import { TypeOperationSaveComponent } from './view/type-operation-save/type-oper
     DecltvacriteriaComponent,
     PopupFactureComponent,
     ViewComponent,
-    OperationSocieteSaveComponent,
-    TypeOperationSaveComponent
+    TypeOperationSaveComponent,
+    CardTypeOperationComponent,
+
+
 
 
   ],
